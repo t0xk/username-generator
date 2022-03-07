08551b68a6f09f62fd05db3b78c1b97c2723c447
nik.volgemut <nik.volgemut@student.um.si>
2022-03-07 21:21:40 +0100

    feat(misc): validateType function Refactored usernameGenerator function added fetchText function skeleton

--- util/methods.js
+++ util/methods.js
@@ -1,18 +1,52 @@
 /**
  * Returns *true* if the element is found in the array, *false* otherwise.
  *
- * @param e The element we are testing for.
+ * @param e An element we are searching for.
  * @param arr The array we are iterating.
  */
 const elementInArray = (e, arr) => {
-    for(let foo of arr){
-        if(e === foo)
+    for (let foo of arr) {
+        if (e === foo)
             return true;
     }
 
     return false;
 }
 
+/**
+ * Checks to see if the type of variable matches the type(s) we pass into the function. Returns true if it does, throws an Error otherwise.
+ * *type* is not validated so the developer is responsible for passing a valid string, or an array of strings.
+ *
+ * @param t {any} - The variable of which we are verifying the type of.
+ * @param type {string} - The type(s) we expect the variable to be.
+ * @return {boolean} - True when types match.
+ */
+const validateType = (t, ...type) => {
+    if (!t || !type)
+        throw new Error("Variable and expected type must both be defined.");
+
+    for (let ty in type) {
+        if (typeof (t) === ty)
+            return true;
+
+    }
+
+    throw new Error(`Variable can't be of type ${typeof (t)}. Expected type(s): ${JSON.stringify(type)}.`);
+}
+
+/**
+ * Fetch content as text from a URL.
+ *
+ * @param url {string} - A string containing a valid URL.
+ */
+const fetchText = (url) => {
+    (async () => {
+
+    })();
+}
+
 module.exports = {
-    elementInArray
+    elementInArray,
+    validateType,
+    fetchText
 }
